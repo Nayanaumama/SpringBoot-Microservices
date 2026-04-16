256ab692ae5af27d714d6eabf54e6813fca70ede
Nayana <Nayana123>
2026-04-16 17:21:05 -0400

    New Initial Commits

---
+++ jwt_toke/src/main/java/com/example/jwt_toke/JwtTokeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_toke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTokeApplication.class, args);
+    }
+
+}
---
+++ jwt_toke/src/test/java/com/example/jwt_toke/JwtTokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_toke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
