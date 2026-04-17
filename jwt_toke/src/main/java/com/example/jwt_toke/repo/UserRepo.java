c824f797fba31b22f19688a9393032e59d9f4d83
Nayana <Nayana123>
2026-04-17 15:51:30 -0400

    for jwt token and Oauth

---
+++ jwt_toke/src/main/java/com/example/jwt_toke/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.jwt_toke.repo;
+
+import com.example.jwt_toke.Model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users,Integer> {
+    Users findByUsername(String username);
+}
---
+++ jwt_toke/src/main/java/com/example/jwt_toke/service/MyUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.example.jwt_toke.service;
+
+import com.example.jwt_toke.Model.Users;
+import com.example.jwt_toke.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    @Autowired
+    UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+
+}
+
+
